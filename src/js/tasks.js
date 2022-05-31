8ddebb5216b11aaf8710e8e18cfb648b6c90000c
jeanCcana <jpcy18@gmail.com>
2022-05-30 22:12:12 -0500

    delete option

--- src/js/tasks.js
+++ src/js/tasks.js
@@ -1,5 +1,9 @@
 import Sortable from "sortablejs";
 import { DateTime } from "luxon";
+import tippy from "tippy.js";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/animations/scale.css";
+
 import defaultGroups from "../assets/default-data";
 
 const boardContent = document.getElementById("board-content");
@@ -8,6 +12,42 @@ const search = document.getElementById("search");
 const setDragCursor = (value) => {
   const html = document.getElementsByTagName("html").item(0);
   html.classList.toggle("grabbing", value);
+};
+
+const eliminar = (taskId) => {
+  let groups = JSON.parse(localStorage.getItem("groups"));
+
+  const filteredGroups = {};
+
+  Object.keys(groups).forEach((key) => {
+    filteredGroups[key] = {
+      ...groups[key],
+      tasks: groups[key].tasks.filter(({ id }) => id !== taskId),
+    };
+  });
+
+  localStorage.setItem("groups", JSON.stringify(filteredGroups));
+  generateGroups(filteredGroups);
+};
+
+const buscar = (searchValue) => {
+  let groups = JSON.parse(localStorage.getItem("groups"));
+  searchValue.trim();
+  if (searchValue !== "") {
+    const filteredGroups = {};
+
+    Object.keys(groups).forEach((key) => {
+      filteredGroups[key] = {
+        ...groups[key],
+        tasks: groups[key].tasks.filter(({ title }) =>
+          title.toLowerCase().includes(searchValue.toLowerCase())
+        ),
+      };
+    });
+    generateGroups(filteredGroups);
+  } else {
+    generateGroups(groups);
+  }
 };
 
 const createSortable = (element, groupName, groups) =>
@@ -20,7 +60,6 @@ const createSortable = (element, groupName, groups) =>
     },
     // sort: false,
     delay: 90,
-    delayOnTouchOnly: true,
     forceFallback: true,
     ghostClass: "sortable-ghost",
     dragClass: "sortable-drag",
@@ -70,6 +109,127 @@ const createSortable = (element, groupName, groups) =>
     },
   });
 
+const generateGroups = (groups) => {
+  const content = Object.keys(groups).map((key) => {
+    const { title: groupTitle, color_class, tasks } = groups[key];
+    return `
+    <div class="flex flex-col gap-4">
+      <div class="flex items-center gap-3">
+        <div class="${color_class} w-4 h-4 rounded"></div>
+        <span class="font-bold">${groupTitle}</span>
+        <i class="fa-solid fa-plus text-gray-400/95 hover-scale-item hover:-translate-y-0 cursor-not-allowed"></i>
+      </div>
+      <div id=${key} class="flex-1 space-y-5 w-60 overflow-x-hidden overflow-y-auto pr-1.5">
+        ${
+          tasks
+            ? tasks
+                .map(
+                  ({
+                    id,
+                    title: taskTitle,
+                    description,
+                    date,
+                    img_url,
+                    participants,
+                  }) => `
+                <!-- Card -->
+                      <div id=${id} class="card">
+                      <!-- Card img -->
+                      ${
+                        img_url
+                          ? `
+                          <!-- Card img -->
+                          <img class="w-full h-24 object-cover rounded-2xl" src=${img_url} alt="">`
+                          : ""
+                      }
+                        <!-- Content -->
+                        <div class="flex gap-1.5">
+                          <div class="flex-1 space-y-1.5">
+                            <span class="font-bold block">${taskTitle}</span>
+                            <span class="text-xs line-clamp-3 text-gray-400/95">
+                            ${description}
+                            </span>
+                          </div>
+                          <i id="${id}-opt" class="fa-solid fa-ellipsis-vertical h-fit text-gray-400/95 text-xl px-1.5 hover-scale-item hover:-translate-y-0">
+                          </i>
+                        </div>
+                        <!-- Details -->
+                        <div class="flex justify-between items-center">
+                          <!-- Date -->
+                          <div class="text-gray-400/95 text-[0.8rem] space-x-1">
+                            <i class="fa-solid fa-calendar-days"></i>
+                            <span>${DateTime.fromISO(date).toFormat("D")}</span>
+                          </div>
+                          <!-- Menbers -->
+                          <div class="flex flex-row items-center -space-x-2">
+                          ${participants
+                            .slice(0, 2)
+                            .map(
+                              ({ img_url }) => `
+                          <img class="w-6 h-6 object-cover border-2 border-white dark:border-[#282A43] rounded-full" src=${img_url} alt="user">
+                          `
+                            )
+                            .join("")}
+                            ${
+                              participants.slice(2).length > 0
+                                ? `
+                            <div
+                              class="w-5 h-5 bg-sky-100 dark:bg-purple-300 flex items-center justify-center rounded-full text-[7px] font-semibold text-black">
+                              +${participants.slice(2).length}
+                            </div>
+                            `
+                                : ""
+                            }
+                          </div>
+                        </div>
+                      </div>
+                `
+                )
+                .join("")
+            : ""
+        }
+        </div>
+    </div>
+    `;
+  });
+
+  boardContent.innerHTML = content.join("");
+
+  Object.keys(groups).forEach((key) => {
+    createSortable(
+      document.getElementById(key),
+      key,
+      Object.keys(groups).filter((item) => item !== key)
+    );
+
+    const { tasks } = groups[key];
+    tasks.forEach(({ id }) => {
+      tippy(`#${id}-opt`, {
+        content: `<div class="space-y-2 text-[0.85rem]">
+                    <span class="block cursor-not-allowed">Editar</span>
+                    <span id="${id}-delete" class="block text-red-500 font-semibold"'>Eliminar</span>
+                  </div>`,
+        interactive: true,
+        arrow: true,
+        placement: "bottom",
+        animation: "scale",
+        trigger: "click",
+        theme: "my-theme",
+        allowHTML: true,
+        onCreate(instance) {
+          instance.popper.querySelector(`#${id}-delete`).onclick = () => {
+            eliminar(id);
+          };
+        },
+      });
+    });
+  });
+};
+
+search.addEventListener("keyup", (e) => {
+  buscar(e.target.value);
+});
+
 const verifyData = () => {
   let groups = JSON.parse(localStorage.getItem("groups"));
   if (groups === null) {
@@ -80,117 +240,4 @@ const verifyData = () => {
   }
 };
 
-const generateGroups = (groups) => {
-  boardContent.innerHTML = "";
-
-  Object.keys(groups).forEach((key) => {
-    const { title, color_class, tasks } = groups[key];
-    const group = document.createElement("div");
-    group.className = "flex flex-col gap-4";
-
-    group.innerHTML = `
-    <div class="flex items-center gap-3">
-      <div class="${color_class} w-4 h-4 rounded"></div>
-      <span class="font-bold">${title}</span>
-      <i class="fa-solid fa-plus text-gray-400/95 hover-scale-item"></i>
-    </div>
-    `;
-
-    const groupTasks = document.createElement("div");
-    groupTasks.setAttribute("id", key);
-    groupTasks.className =
-      "flex-1 space-y-5 w-60 overflow-x-hidden overflow-y-auto pr-1.5";
-
-    if (tasks !== null) {
-      tasks.forEach(
-        ({ id, title, description, date, img_url, participants }) => {
-          groupTasks.innerHTML += `
-      <!-- Card -->
-            <div id=${id} class="card">
-            <!-- Card img -->
-            ${
-              img_url
-                ? `
-                <!-- Card img -->
-                <img class="w-full h-24 object-cover rounded-2xl" src=${img_url} alt="">`
-                : ""
-            }
-              <!-- Content -->
-              <div class="flex gap-1.5">
-                <div class="flex-1 space-y-1.5">
-                  <span class="font-bold block">${title}</span>
-                  <span class="text-xs line-clamp-3 text-gray-400/95">
-                  ${description}
-                  </span>
-                </div>
-                <i class="fa-solid fa-ellipsis-vertical text-gray-400/95 text-xl"></i>
-              </div>
-              <!-- Details -->
-              <div class="flex justify-between items-center">
-                <!-- Date -->
-                <div class="text-gray-400/95 text-[0.8rem] space-x-1">
-                  <i class="fa-solid fa-calendar-days"></i>
-                  <span>${DateTime.fromISO(date).toFormat("D")}</span>
-                </div>
-                <!-- Menbers -->
-                <div class="flex flex-row items-center -space-x-2">
-                ${participants
-                  .slice(0, 2)
-                  .map(
-                    ({ img_url }) => `
-                <img class="w-6 h-6 object-cover border-2 border-white dark:border-[#282A43] rounded-full" src=${img_url} alt="user">
-                `
-                  )
-                  .join("")}
-                  ${
-                    participants.slice(2).length > 0
-                      ? `
-                  <div
-                    class="w-5 h-5 bg-sky-100 dark:bg-purple-300 flex items-center justify-center rounded-full text-[7px] font-semibold text-black">
-                    +${participants.slice(2).length}
-                  </div>
-                  `
-                      : ""
-                  }
-                </div>
-              </div>
-            </div>
-      `;
-        }
-      );
-    }
-
-    createSortable(
-      groupTasks,
-      key,
-      Object.keys(groups).filter((item) => item !== key)
-    );
-
-    group.appendChild(groupTasks);
-
-    boardContent.appendChild(group);
-  });
-};
-
 verifyData();
-
-search.addEventListener("keyup", (e) => {
-  let groups = JSON.parse(localStorage.getItem("groups"));
-
-  const searchValue = e.target.value.trim();
-  if (searchValue !== "") {
-    const filteredGroups = {};
-
-    Object.keys(groups).forEach((key) => {
-      filteredGroups[key] = {
-        ...groups[key],
-        tasks: groups[key].tasks.filter(({ title }) =>
-          title.toLowerCase().includes(searchValue.toLowerCase())
-        ),
-      };
-    });
-    generateGroups(filteredGroups);
-  } else {
-    generateGroups(groups);
-  }
-});
